455f5202807ecd09905c166da123c9bb2a098a29
michaldanielewicz <michaldanielewicz94@gmail.com>
2021-10-06 12:59:28 +0100

    add create_superuser method

--- app/core/models.py
+++ app/core/models.py
@@ -5,9 +5,19 @@ from django.contrib.auth.models import AbstractUser, AbstractBaseUser, BaseUserM
 class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
-        """Creates and saves a new users."""
-        user = self.model(email=email, **extra_fields)
+        """Creates and saves a new user."""
+        if not email:
+            raise ValueError("Users must have email address")
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password):
+        """Creates ans saves a new super user."""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
         user.save(using=self._db)
         return user
 
@@ -20,5 +30,3 @@ class User(AbstractBaseUser, PermissionsMixin):
     is_staff = models.BooleanField(default=False)
     objects = UserManager()
     USERNAME_FIELD = "email"
-
-
